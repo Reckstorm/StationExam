25e150e76378daa2561da15715831c17e68075d0
Mihail Sevagin <lolvillian@gmail.com>
2023-05-25 00:27:41 +0300

    Added user CRUD logics

--- ServiceWindow.cs
+++ ServiceWindow.cs
@@ -2,16 +2,26 @@ using Dapper;
 using StationExam.Models;
 using StationExam.Models.Essentials;
 using System.Data.SqlClient;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace StationExam
 {
     public partial class ServiceWindow : Form
     {
-        public Login login = new Login() { Dock = DockStyle.Fill };
+        public Login login;
         public string connection = "Server=localhost\\SQLEXPRESS;Database=Station;Trusted_Connection=True;TrustServerCertificate=True;";
         public List<Users> users = new List<Users>();
+        public List<Roles> roles = new List<Roles>();
         public Users currentUser;
+
+        UserItem userItem = new UserItem();
+
+        Panel left = new Panel();
+        Panel right = new Panel();
+
+        Label userName = new Label();
+        Button logOut = new Button();
+
+        Button manageClients = new Button();
         public ServiceWindow()
         {
             InitializeComponent();
@@ -26,14 +36,15 @@ namespace StationExam
                         "JOIN Logins ON Users.Id = Logins.Id " +
                         "JOIN Passwords ON Users.Id = Passwords.Id").ToList();
                 }
+                login = new Login() { Dock = DockStyle.Fill };
                 this.Controls.Add(login);
-
             };
         }
 
         public void RenderUserUI(Users user)
         {
-
+            ClientManagement();
+            OrderManagement(user);
         }
 
         public void EmployeeManagement()
@@ -43,12 +54,53 @@ namespace StationExam
 
         public void ClientManagement()
         {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                roles = conn.Query<Roles>("SELECT * FROM ROLES").ToList();
+            }
 
+            userItem.Location = new Point(0, 0);
+
+            manageClients.Text = "Manage Clients";
+            manageClients.Height = 50;
+            manageClients.Dock = DockStyle.Top;
+            manageClients.Click += (s, e) => right.Controls.Add(userItem);
+            left.Controls.Add(manageClients);      
         }
 
-        public void OrderManagement()
+        public void OrderManagement(Users user)
         {
+            #region Initialize Layout
+            userName.Text = $"Hello {user.Name}";
+            userName.Dock = DockStyle.Top;
 
+            logOut.Text = "Logout";
+            logOut.Height = 50;
+            logOut.Dock = DockStyle.Top;
+            logOut.Click += (s, e) =>
+            {
+                this.Controls.Clear();
+                login = new Login() { Dock = DockStyle.Fill };
+                this.Controls.Add(login);
+            };
+
+            left.Location = new Point(0, 0);
+            left.Width = 300;
+            left.Height = this.Height - 50;
+            left.BackColor = Color.LightGreen;
+
+            right.Location = new Point(left.Width, 0);
+            right.BackColor = Color.LightSkyBlue;
+            right.Width = this.Width - left.Width - 20;
+            right.Height = this.Height - 50;
+
+            left.Controls.Add(logOut);
+            left.Controls.Add(userName);
+
+            this.Controls.Add(left);
+            this.Controls.Add(right);
+            #endregion
         }
     }
 }
---
+++ UserItem.cs
@@ -0,0 +1,108 @@
+﻿using Dapper;
+using Microsoft.VisualBasic.ApplicationServices;
+using StationExam.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StationExam
+{
+    public partial class UserItem : UserControl
+    {
+        public UserItem()
+        {
+            InitializeComponent();
+            Load += DefaultData;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Err_lb.Text = string.Empty;
+            ServiceWindow parent = this.Parent.Parent as ServiceWindow;
+
+            if (parent.users.Any(user => user.Id == int.Parse(this.Id_lb.Text)))
+            {
+                using (SqlConnection conn = new SqlConnection(parent.connection))
+                {
+                    conn.Open();
+                    conn.Execute("UPDATE [Users] SET Name = @Name, Role = @Role WHERE Id = @Id",
+                        new { Name = this.Name_tb.Text, Role = parent.roles.FindIndex(x => x.Role.Equals(this.Role_cb.SelectedItem)) + 1, Id = int.Parse(this.Id_lb.Text) });
+                    conn.Execute("UPDATE [Logins] SET Login = @Login WHERE Login = @OldLogin",
+                        new { Login = this.Login_tb.Text, OldLogin = parent.users.Find(user => user.Id.Equals(this.Id_lb)).Login });
+                    conn.Execute("UPDATE [Passwords] SET Password = @Password WHERE Password = @OldPassword",
+                        new { Password = this.Password_tb.Text, OldPassword = parent.users.Find(user => user.Id.Equals(this.Id_lb)).Password });
+                    this.Err_lb.ForeColor = Color.Green;
+                    this.Err_lb.Text = "User updated";
+                }
+            }
+            else
+            {
+                if (parent.users.Any(user => user.Login.Equals(this.Login_lb.Text)))
+                {
+                    this.Err_lb.Text = "Login is already taken";
+                    return;
+                }
+                using (SqlConnection conn = new SqlConnection(parent.connection))
+                {
+                    conn.Open();
+
+                    conn.Execute("INSERT INTO [Logins] ([Login]) VALUES (@Login)",
+                        new { Login = this.Login_tb.Text });
+
+                    conn.Execute("INSERT INTO [Passwords] ([Password]) VALUES (@Password)",
+                         new { Password = this.Password_tb.Text });
+
+                    conn.Execute("INSERT INTO [Users] ([Name], [RoleID], [LoginID], [PasswordID]) VALUES (@Name, @Role, @Id, @Id)",
+                        new
+                        {
+                            Id = int.Parse(this.Id_lb.Text),
+                            Name = this.Name_tb.Text,
+                            Role = parent.roles.FindIndex(x => x.Role.Equals(this.Role_cb.SelectedItem)) + 1,
+                        });
+                    this.Err_lb.ForeColor = Color.Green;
+                    this.Err_lb.Text = "User created";
+                }
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            this.Err_lb.Text = string.Empty;
+            ServiceWindow parent = this.Parent.Parent as ServiceWindow;
+
+            if (parent.users.Any(user => user.Id == int.Parse(this.Id_lb.Text)))
+            {
+                using (SqlConnection conn = new SqlConnection(parent.connection))
+                {
+                    conn.Open();
+                    conn.Execute("DELETE FROM [Users] WHERE Id = @Id", new { Id = int.Parse(this.Id_lb.Text) });
+                    conn.Execute("DELETE FROM [Logins] WHERE Id = @Id", new { Id = int.Parse(this.Id_lb.Text) });
+                    conn.Execute("DELETE FROM [Passwords] WHERE Id = @Id", new { Id = int.Parse(this.Id_lb.Text) });
+                    this.Err_lb.ForeColor = Color.Green;
+                    this.Err_lb.Text = "User deleted";
+                }
+            }
+            else
+            {
+                this.Err_lb.ForeColor = Color.Red;
+                this.Err_lb.Text = "There is no such user";
+            }
+        }
+
+        private void DefaultData(object sender, EventArgs e)
+        {
+            ServiceWindow parent = this.Parent.Parent as ServiceWindow;
+            parent.roles.ForEach(role => this.Role_cb.Items.Add(role.Role));
+            this.Id_lb.Text = (parent.users.Last().Id + 1).ToString();
+            this.Err_lb.Text = string.Empty;
+        }
+
+    }
+}
